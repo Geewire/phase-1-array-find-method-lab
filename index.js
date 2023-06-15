021ffab02dc245e5ed21d70d72412be7b49e1947
root <root@DESKTOP-6I3RBRC>
2023-06-15 23:02:41 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+
+function superbowlWin(record){
+    const win = record.find(collection => collection.result === "W");
+    console.log(win)
+    if (win) {
+         return win.year;
+    }else {
+        return undefined;
+}
+}
